64eaa9807da8f960ad4bdaefb3121aea5eff90f8
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-02 15:55:59 +0500

    s1 va s2 matin larni uzunliklarinio qushish

--- bin/string_length05.dart
+++ bin/string_length05.dart
@@ -7,6 +7,12 @@
     Returns:
         total length of strings
 */
-void main() {
-  // write your code here
+int func(String s1,s2){
+    int x,y;
+    x=s1.length;
+    y=s2.length;
+    return x+y;
+}
+void main(){
+    print(func('Salohiddin','Berdiyorov'));
 }
