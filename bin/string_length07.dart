4cd9c978773eeb2698c03c7e875b05edbabfa7e2
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-02 16:22:23 +0500

    s1 ni uzunligi juft bulsa s1 ni chiqaring aks xolda s2 ni chiqarig

--- bin/string_length07.dart
+++ bin/string_length07.dart
@@ -7,6 +7,14 @@
     Returns:
         string
 */
-void main() {
-  // write your code here
+String func(String s1,s2){
+    int x;
+    x=s1.length;
+    if(x%2==0){
+      return s1;
+    }else
+      return s2;
+}
+void main(){
+    print(func('nicee','lion'));
 }
