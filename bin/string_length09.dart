188c62936d487e2bd923df021bc5f29acfae9479
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-02 16:41:03 +0500

    s1+s2  aks xolda s1 ni chiqarish

--- bin/string_length09.dart
+++ bin/string_length09.dart
@@ -7,6 +7,15 @@
     Returns:
         str: answer
      */
-void main() {
-  // write your code here
+String func(String s1,s2){
+    int x,y;
+    x=s1.length;
+    y=s2.length;
+    if(x>y){
+      return s1+s2;
+    }else
+      return s1;
+}
+void main(){
+    print(func('Berdiyorovv','Salohiddin'));
 }
